eb3818ce61e376720afb285c3f7adfae4e02695a
Gabriel Sá <50773849+GabrielDesmodus@users.noreply.github.com>
2020-08-27 15:35:16 -0300

    Initial commit

---
+++ src/Arquivo.java
@@ -0,0 +1,42 @@
+
+public class Arquivo {
+	public class DataFile {
+
+	    private File file;
+
+	    public DataFile(String fileName) {
+	        this.file = new File(fileName);
+	    }
+
+	    public DataFile(File file) {
+	        this.file = file;
+	    }
+
+	    public void save(Person person) {
+	        try(BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+	            writer.write(person.getFirstName()+"\r\n" + person.getSurname() + "\r\n" + person.getPhoneNumber() + "\r\n" + person.getEmail() +
+	                    "\r\n" + person.getAddress() + "\r\n\r\n");
+	        } catch(IOException e) {
+	            System.out.println(e);
+	        }
+	    }
+
+	    public List<Person> loadAll() throws IOException {
+	        List<Person> people = new ArrayList<Person>();
+
+	        try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
+	            String name = null;
+	            while((name = reader.readLine()) != null) {
+	                Person person = new Person(name, reader.readLine(), reader.readLine(), reader.readLine(), reader.readLine());
+	                people.add(person);
+	                reader.readLine();
+	            }
+	        }
+	        catch ( IOException e) {
+	            System.out.println(e);
+	        }
+
+	        return people;
+	    }
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+	  public static void main(String[] args) {
+	         AddressBookController controller = new AddressBookController("data_file.txt");
+	         controller.run();
+	     }
+}
